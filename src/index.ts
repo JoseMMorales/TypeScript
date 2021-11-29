a44a7e951a68468f93c8201d49376682c07d6c89
JoseMMorales <Jose.Mula@met.ey.com>
2021-11-29 12:09:32 +0100

    Objects and TypeScript Alias

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-// Hello World Message with TypeScript
+// Hello World Message with TypeScript----------------------------
 
 let message: string = 'Hello world';
 message += ' again';
 console.log(message);
 
 
-// Primitive Types
+// Primitive Types------------------------------------------------
 
 let isPresent: boolean = false;
 let magic: number = 66.6;
@@ -16,24 +16,13 @@ let notPresent: null = null;
 
 let penta: symbol = Symbol('star');
 
-//Instance Types
+//Instance Types---------------------------------------------------
 
 let rerexp: RegExp = new RegExp('ab+c');
-
 let array: Array<number> = [1, 2, 3];
-
 let set: Set<number> = new Set([1, 2, 3])
 
-// A first in first out collection Class
-class Queue<T> {
-    private data: Array<T> = [];
-    push(item: T) { this.data.push(item) }
-    pop(): T | undefined { return this.data.shift() }
-}
-
-let queue: Queue<number> = new Queue();
-
-// Arrays and Tuples
+// Arrays and Tuples-----------------------------------------------
 
 // Array Type Shortcut
 let numberArray: number[] = [1, 2, 3];
@@ -54,5 +43,19 @@ tuple = [2, 8]; //Ok
 //tuple = [5]; !!Error occurred, as not two member of Array are assigned
 //tuple = [5, 6, 7]; !!Error occurred, as not two member of Array are assigned
 //tuple = ['string', 3]; !!Error occurred, as not two able to assign to a number (As string)
+
+//Object Types and Type Aliases-----------------------------------------------
+
+let center: { x: number, y: number } = {
+    x: 0,
+    y: 0,
+};
+
+type Point = { x: number, y: number };
+
+let unit: Point = {
+    x: 0,
+    y: 0,
+};
 
 
