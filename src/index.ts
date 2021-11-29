b698bb8e65a9fa27ec6943c4c3fe2fd9cd9fde0b
JoseMMorales <Jose.Mula@met.ey.com>
2021-11-29 17:56:31 +0100

    Literal Types

--- src/index.ts
+++ src/index.ts
@@ -288,3 +288,17 @@ console.log(formatCommandLine('Hello', 4)); // '     Hello'
 console.log(formatCommandLine('Hello', ' ')); // '  Hello'
 console.log(formatCommandLine('Hello', '---')); //'-----Hello'
 //console.log(formatCommandLine('Hello', false)); //'Error'
+
+// Literal Types----------------------------------------------------------------
+
+type DiceValue = 1 | 2 | 3 | 4 | 5 | 6;
+
+function rollDice() {
+    return (Math.floor(Math.random() * 6) + 1) as DiceValue;
+}
+
+/* Error as 7 is not defined in type DiceValue
+*   if (rollDice() === 7) {
+*       throw new Error('Not possible!');
+*   }
+*/
