73fcb78a4dded792711baeeca4f8d22d9751091d
JoseMMorales <Jose.Mula@met.ey.com>
2021-11-29 11:44:21 +0100

    Instance types

--- src/index.ts
+++ src/index.ts
@@ -15,4 +15,20 @@ let notDefined: undefined = undefined;
 let notPresent: null = null;
 
 let penta: symbol = Symbol('star');
-//let biggy: bigint = 24n;
+
+//Instance Types
+
+let rerexp: RegExp = new RegExp('ab+c');
+
+let array: Array<number> = [1, 2, 3];
+
+let set: Set<number> = new Set([1, 2, 3])
+
+// A first in first out collection Class
+class Queue<T> {
+    private data: Array<T> = [];
+    push(item: T) { this.data.push(item) }
+    pop(): T | undefined { return this.data.shift() }
+}
+
+let queue: Queue<number> = new Queue();
