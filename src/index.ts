9846690e0975c9f8dea6530922e86c815c6f0304
JoseMMorales <Jose.Mula@met.ey.com>
2021-11-29 11:29:39 +0100

    Switching from es2016 to es2020

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
+// Hello World Message with TypeScript
+
 let message: string = 'Hello world';
+message += ' again';
 console.log(message);
+
+
+// Primitive Types
+
+let isPresent: boolean = false;
+let magic: number = 66.6;
+let hello: string = 'world';
+
+let notDefined: undefined = undefined;
+let notPresent: null = null;
+
+let penta: symbol = Symbol('star');
+//let biggy: bigint = 24n;
