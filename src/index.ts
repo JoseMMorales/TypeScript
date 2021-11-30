8fc62299d88799902af344752ebf5e83c5675462
JoseMMorales <Jose.Mula@met.ey.com>
2021-11-30 16:09:51 +0100

    This Parameter

--- src/index.ts
+++ src/index.ts
@@ -770,3 +770,25 @@ dave.name = 'grohl'; // Error is readonly
 dave.role = 'singer'; // Error is readonly
 dave.skills.unshift('singing'); // Error is readonly
 */
+
+// This Parameter----------------------------------------------------------
+
+function double(this: { value: number }) {
+    this.value = this.value * 2;
+}
+
+const valid = {
+    value: 10,
+    double,
+}
+
+valid.double();
+
+console.log(valid.double); // 20
+
+const invalid = {
+    value: 10,
+    double,
+};
+
+invalid.double();
