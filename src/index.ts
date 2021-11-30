aae427584b862c6af3ce3c9519b910b7cbe3e013
JoseMMorales <Jose.Mula@met.ey.com>
2021-11-30 16:27:04 +0100

    Generic Constrains

--- src/index.ts
+++ src/index.ts
@@ -792,3 +792,26 @@ const invalid = {
 };
 
 invalid.double();
+
+// Generic Constrains--------------------------------------------------------
+
+type NameFields = { firstName: string, lastName: string };
+
+function addFullName<T extends NameFields>(obj: T): T & { fullName: string } {
+    return {
+        ...obj,
+        fullName: `${obj.firstName} ${obj.lastName}`,
+    }
+};
+
+const john = addFullName({
+    email:  'john@gmail.com',
+    firstName: 'John',
+    lastName: 'Doe',
+});
+
+console.log(john.email); // john@gmail.com
+console.log(john.fullName); // John Doe
+
+const jane = addFullName({ firstName: 'Jane', lastName: 'Smith' });
+
