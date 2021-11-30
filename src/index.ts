223652ca5ab2070a72d5933bda9a5e26fa104587
JoseMMorales <Jose.Mula@met.ey.com>
2021-11-30 13:31:31 +0100

    Abstract Classes

--- src/index.ts
+++ src/index.ts
@@ -660,3 +660,30 @@ const adde: Adde = (a: number, b: number) => {
     return a + b;
 };
 add.debbugName = 'Addition Function';
+
+// Abstract Classes--------------------------------------------------------------------
+
+abstract class Command {
+    abstract commandLine(): string;
+
+    execute() {
+        console.log('Executing:', this.commandLine());        
+    }
+}
+
+class GitResetCommand extends Command {
+    commandLine() {
+        return 'git reset --hard';
+    }
+}
+
+class GitFetchCommand extends Command {
+    commandLine() {
+        return 'git fetch --all';
+    }
+}
+
+new GitResetCommand().execute();
+new GitFetchCommand().execute();
+
+//new Command(); //Cannot create an instance of an abstract class.
