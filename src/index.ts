9d25da093df4f9496908263059e9d213dcb4066b
JoseMMorales <Jose.Mula@met.ey.com>
2021-12-13 10:08:45 +0100

    Conditional Types with Union

--- src/index.ts
+++ src/index.ts
@@ -961,3 +961,32 @@ const obj = typeName(null);
 
 console.log(typeof null);
 
+// Conditional Types with Union-----------------------------------------------------
+
+function error(message: string): never {
+    throw new Error(message);
+}
+
+//const notAllowed: never = 'some string';
+const allowed: never = error('this is ok');
+const example: string = error('it will not return');
+
+type Verbose = string | never;
+type Concise = string;
+
+/**
+ * Exclude null and undefined from T
+ */
+export type NoEmpty<T> = T extends null | undefined ? never : T;
+
+type Example = NoEmpty<string | null>;
+type Expanded0 = NoEmpty<string> | NoEmpty<null>;
+type Expanded1 = (string extends null | undefined ? never : string)
+                    | (null extends null | undefined ? never : string);
+type Expanded2 = string | never;
+type Result = string;
+
+
+
+
+
