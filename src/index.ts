0ed7a066af96c1bf1fe099029108dc6484309aea
JoseMMorales <Jose.Mula@met.ey.com>
2021-12-13 10:22:54 +0100

    Infer keyword and

--- src/index.ts
+++ src/index.ts
@@ -986,7 +986,42 @@ type Expanded1 = (string extends null | undefined ? never : string)
 type Expanded2 = string | never;
 type Result = string;
 
+// Infer keyword and `ReturnType<T>` -------------------------------------------------
 
+type isArray<T> =
+    T extends Array<any>
+    ? 'array'
+    : 'other';
 
+type WithArray = isArray<string[]>;
+type WithNoArray = isArray<number>;
+
+type UnboxArray<T> = 
+ T extends Array<infer Member>
+ ? Member 
+ : T;
+
+ type UnboxedStringArray = UnboxArray<string[]>;
+ type UnboxedNumberArray = UnboxArray<number[]>;
+ type AnythingElse = UnboxArray<string>;
+
+ // Antoher Real Example ------------------------------------------------------------------
+
+ export function createPerson (firstName: string, lastName: string) {
+     return {
+         firstName,
+         lastName,
+         fullName: `${firstName} ${lastName}`
+     };
+ }
+
+ function logPerson(person: ReturnType<typeof createPerson>) {
+     console.log(
+         'Person:',
+         person.firstName,
+         person.lastName,
+         person.fullName
+     );
+ }
 
 
